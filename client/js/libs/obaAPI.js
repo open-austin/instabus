b24826201961703b459184c1f72bd7687cb9333f
Luqmaan <ldawoodjee@gmail.com>
2016-02-13 11:34:57 -0600

    ;_;

--- client/js/libs/obaAPI.js
+++ client/js/libs/obaAPI.js
@@ -4,7 +4,7 @@ import queryString from 'query-string';
 
 export default function request(endpoint, query = {}) {
   // const url = `http://localhost:8080/api/where/${endpoint}.json`;
-  const url = `http://52.35.48.91:8080/api/where/${endpoint}.json`;
+  const url = `http://52.88.82.199:8080/onebusaway-api-webapp/api/where/${endpoint}.json`;
   // const url = `http://api.tampa.onebusaway.org/api/where/${endpoint}.json`;
   console.log('url', url)
   const qs = queryString.stringify({
