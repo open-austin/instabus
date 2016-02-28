6c55791c6b02947f12eae762c3352772036b4074
Luqmaan <ldawoodjee@gmail.com>
2016-02-28 14:07:55 -0600

    Forgot dispatch

--- client/js/index.js
+++ client/js/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import Root from './containers/Root';
-import configureStore from './store/configureStore';
-
+import Root from 'js/containers/Root';
+import configureStore from 'js/store/configureStore';
+import {getRoutes} from 'js/actions/data';
 
 const store = configureStore();
 window.store = store;
@@ -14,7 +14,7 @@ render(
 );
 
 // store.dispatch(actions.ui.getUserLatLng());
-// store.dispatch(actions.data.getRoutes());
+store.dispatch(getRoutes());
 
 // store.dispatch(actions.data.getStopsForLocation(userLatLng));
 // store.dispatch(actions.ui.setRoute('Hillsborough Area Regional Transit_6'));
