85abd0a8169014547edf3ef497d779d03697f0aa
Luqmaan <ldawoodjee@gmail.com>
2016-03-25 08:40:26 -0400

    Fix typo

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist', {
+ghpages.publish(path.join(__dirname, 'dist'), {
   repo: 'https://' + process.env.GH_TOKEN + '@github.com/open-austin/instabus.git',
   user: {
     name: 'Travis CI',
     email: 'hack@open-austin.org',
   },
-}), function (err) {
+}, function (err) {
   if (err) {
     console.error(err);
   }
