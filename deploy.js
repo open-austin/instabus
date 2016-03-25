de56db697d1064cfc15def69d8dd289b36a54db7
Luqmaan <ldawoodjee@gmail.com>
2016-03-25 08:37:54 -0400

    Add secure GH_TOKEN to deploy

--- deploy.js
+++ deploy.js
@@ -1,9 +1,15 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist'), function (err) {
+ghpages.publish(path.join(__dirname, 'dist', {
+  repo: 'https://' + process.env.GH_TOKEN + '@github.com/open-austin/instabus.git',
+  user: {
+    name: 'Travis CI',
+    email: 'hack@open-austin.org',
+  },
+}), function (err) {
   if (err) {
-    console.log(err);
+    console.error(err);
   }
   else {
     console.log('available on gh page');
