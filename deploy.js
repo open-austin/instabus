aa831c73179fe92f22c4975cf5071e14996eb22a
Luqmaan <ldawoodjee@gmail.com>
2016-04-19 20:54:34 -0500

    Fix deploy to work locally

--- deploy.js
+++ deploy.js
@@ -1,13 +1,15 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'dist'), {
+const options = !process.env.GH_TOKEN ? null : {
   repo: 'https://' + process.env.GH_TOKEN + '@github.com/open-austin/instabus.git',
   user: {
     name: 'Travis CI',
     email: 'hack@open-austin.org',
   },
-}, function (err) {
+};
+
+ghpages.publish(path.join(__dirname, 'dist'), options, function (err) {
   if (err) {
     console.error(err);
   }
